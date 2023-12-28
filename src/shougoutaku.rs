56c96ea1395c26b5152b8049c79a2ea5d5e633ef
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2023-12-28 22:35:27 +0900

    adding clap for argument parsing

--- src/shougoutaku.rs
+++ src/shougoutaku.rs
@@ -4,12 +4,11 @@ mod trade_matcher;
 
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
 use log::{info, debug, error};
 use rust_decimal::Decimal;
 use std::error::Error;
 use std::fmt;
-
+use clap::{Command, Arg};
 
 use messages::{SnapShotUpdate, TradeUpdate, DepthUpdate};
 use orderbook::OrderBook;
@@ -78,29 +77,63 @@ fn next_trades(trade_reader: &mut Reader, prev_trade_info: &mut PreviousTradeInf
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     info!("Starting Matching Engine");
     env_logger::init();
-    // Load the snapshot from the specified path
-    let snapshot_path = Path::new("data/capture/btcusdt/1702798595534677/snapshot.txt");
-    let snapshot_file = File::open(snapshot_path)?;
-    let snapshot_reader = io::BufReader::new(snapshot_file);
-    let snapshot: SnapShotUpdate = serde_json::from_reader(snapshot_reader)?;
 
-    debug!("Snapshot loaded: {:?}", snapshot);
+    let matches = Command::new("My Trading Application")
+        .version("1.0")
+        .author("Author Name <author@example.com>")
+        .about("Does awesome trade matching")
+        .arg(Arg::new("snapshot")
+             .short('s')
+             .long("snapshot")
+             .value_name("PATH_TO_SNAPSHOT")
+             .help("Sets the snapshot file path")
+             .required(true))  // Making this argument required
+        .arg(Arg::new("ask_trade")
+             .short('a')
+             .long("ask_trade")
+             .value_name("PATH_TO_ASK_TRADE")
+             .help("Sets the ask trade file path")
+             .required(true))  // Making this argument required
+        .arg(Arg::new("bid_trade")
+             .short('b')
+             .long("bid_trade")
+             .value_name("PATH_TO_BID_TRADE")
+             .help("Sets the bid trade file path")
+             .required(true))  // Making this argument required
+        .arg(Arg::new("depth")
+             .short('d')
+             .long("depth")
+             .value_name("PATH_TO_DEPTH")
+             .help("Sets the depth file path")
+             .required(true))  // Making this argument required
+        .get_matches();
 
-    // Open the files using the specified paths
-    let ask_trade_file = File::open("data/capture/btcusdt/1702798595534677/ask_trade.txt")?;
-    let bid_trade_file = File::open("data/capture/btcusdt/1702798595534677/bid_trade.txt")?;
-    let depth_file = File::open("data/capture/btcusdt/1702798595534677/depth.txt")?;
+    // Safely accessing the argument values
+    let snapshot_file_path = matches.get_one::<String>("snapshot").unwrap();
+    let ask_trade_file_path = matches.get_one::<String>("ask_trade").unwrap();
+    let bid_trade_file_path = matches.get_one::<String>("bid_trade").unwrap();
+    let depth_file_path = matches.get_one::<String>("depth").unwrap();
+
+
+    let snapshot_file = File::open(snapshot_file_path)?;
+    let ask_trade_file = File::open(ask_trade_file_path)?;
+    let bid_trade_file = File::open(bid_trade_file_path)?;
+    let depth_file = File::open(depth_file_path)?;
 
     // Create buffered readers for each file
+    let snapshot_reader = io::BufReader::new(snapshot_file);
     let mut ask_trade_reader = io::BufReader::new(ask_trade_file).lines();
     let mut bid_trade_reader = io::BufReader::new(bid_trade_file).lines();
     let mut depth_reader = io::BufReader::new(depth_file).lines();
 
-    let mut next_ask_trade = true;
-    let mut next_bid_trade = true;
+    let snapshot: SnapShotUpdate = serde_json::from_reader(snapshot_reader)?;
+    debug!("Snapshot loaded: {:?}", snapshot);
 
     let mut orderbook = OrderBook::new();
     orderbook.update_with_snapshot(snapshot);
+
+    let mut next_ask_trade = true;
+    let mut next_bid_trade = true;
 
     let mut bid_matcher = TradeMatcher::new(TradeType::Bid);
     let mut ask_matcher = TradeMatcher::new(TradeType::Ask);
@@ -116,7 +149,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 break; // Break out of the loop
             }
         }
-        if ask_matcher.number_of_timestamps() == 5 {
+        if ask_matcher.number_of_timestamps() == 3 {
             break;
         }
     }
@@ -128,7 +161,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 break; // Break out of the loop
             }
         }
-        if bid_matcher.number_of_timestamps() == 5 {
+        if bid_matcher.number_of_timestamps() == 3 {
             break;
         }
     }
@@ -153,7 +186,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         break; // Break out of the loop
                     }
                 }
-                if ask_matcher.number_of_timestamps() == 5 {
+                if ask_matcher.number_of_timestamps() == 3 {
                     break;
                 }
             }
@@ -169,7 +202,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         break; // Break out of the loop
                     }
                 }
-                if bid_matcher.number_of_timestamps() == 5 {
+                if bid_matcher.number_of_timestamps() == 3 {
                     break;
                 }
             }
